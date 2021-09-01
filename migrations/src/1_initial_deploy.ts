3bd1f33be59934abdd8a20eead7bcb0f7e4366f3
Safwan Shaheer <devorein00@gmail.com>
2021-09-01 20:10:37 +0600

    Added typedefs for migration code

--- migrations/src/1_initial_deploy.ts
+++ migrations/src/1_initial_deploy.ts
@@ -1,6 +1,8 @@
-const ProductAuth = (global as any).artifacts.require("ProductAuth");
+import { ProductAuthContract } from "../../types/truffle-contracts";
 
-module.exports = function deploy(deployer: any) {
+const ProductAuth = (global as any).artifacts.require("ProductAuth") as ProductAuthContract;
+
+module.exports = function deploy(deployer: Truffle.Deployer) {
   deployer.deploy(ProductAuth);
   console.log("Successfully deployed ProductAuth smart contract");
-};
+} as Truffle.Migration;
