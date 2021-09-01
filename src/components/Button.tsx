a9b86f887b4f24253672bd5b9ad12c130d16cccd
Safwan Shaheer <devorein00@gmail.com>
2021-09-01 20:41:59 +0600

    Removed funding references

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 
 export function Button(props: ButtonProps) {
   const { content, onClick, disabled = false, variant = "primary" } = props;
-  const primaryVariantClasses = `text-white bg-gray-900 ${disabled ? "" : "hover:bg-white hover:text-black border-gray-900"}`, secondaryVariantClasses = `text-black bg-white border-gray-900 ${disabled ? "" : "hover:bg-gray-900 hover:text-white"}`;
+  const primaryVariantClasses = `text-white bg-gray-900 ${disabled ? "" : "hover:bg-white hover:text-black border-gray-900"}`, secondaryVariantClasses = `text-black bg-white ${disabled ? "" : "hover:bg-gray-900 hover:border-white hover:text-white"}`;
   const variantClasses = variant === "primary" ? primaryVariantClasses : secondaryVariantClasses;
 
   return <button disabled={disabled} onClick={onClick} type="button" className={`Button ${variantClasses} m-2 rounded-sm block shadow-border disabled:bg-gray-400  transition-colors duration-200 text-base py-2 px-5 uppercase font-semibold ${disabled ? "" : "border-2"}`}>
